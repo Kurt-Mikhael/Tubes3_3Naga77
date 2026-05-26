79a90f9afd5b1f1c8682da3e5146d981fe80a05e
Andra <angelinaandra93@gmail.com>
2026-05-26 15:49:33 +0700

    fix: regex

--- src/algorithms/regex.ts
+++ src/algorithms/regex.ts
@@ -3,13 +3,12 @@ import { RegexRes } from "../types/type"
 export function regexSearch(text: string): RegexRes[] {
     const res: RegexRes[] = [];
     
-    const pattern = new RegExp('\\b[A-Za-z]{2,}\\d{2,}\\b', 'gi');
+    const pattern = new RegExp('\\b[A-Za-z]{2,}\\d{2,3}\\b', 'gi');
     const matches = text.matchAll(pattern);
 
     for (const match of matches) {
         const full = match[0];
         
-        // Pisahkan bagian kata dan angkanya
         const wordPart = full.replace(/\d+$/, '');
         res.push({
             matchedWord: full,

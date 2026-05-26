5991ea0aafd934907004464c6c7c9328e3829f0a
Kurt-Mikhael <kurtmikhael123@gmail.com>
2026-05-26 18:04:53 +0700

    Fix: load concept from per keyword to line

--- src/algorithms/regex.ts
+++ src/algorithms/regex.ts
@@ -3,7 +3,7 @@ import { RegexRes } from "../types/type"
 export function regexSearch(text: string): RegexRes[] {
     const res: RegexRes[] = [];
     
-    const pattern = new RegExp('\\b[A-Za-z]{2,}\\d{2,3}\\b', 'gi');
+    const pattern = new RegExp('\\b[A-Za-z]{2,}\\d{2,}\\b', 'gi');
     const matches = text.matchAll(pattern);
 
     for (const match of matches) {
